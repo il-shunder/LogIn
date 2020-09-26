3b93decce21eba0d72c6a7281a3ec39cc9c38d3d
il-shunder <70568857+il-shunder@users.noreply.github.com>
2020-09-26 11:19:53 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -10,14 +10,14 @@ $pass = $_POST['password'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'rokgrupa0@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '1rjkjvbz'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'mibrig@bk.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'DOrayaaOA12~'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('rokgrupa0@gmail.com'); // от кого будет уходить письмо?
+$mail->setFrom('mibrig@bk.ru'); // от кого будет уходить письмо?
 $mail->addAddress('ilshunder2003@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
